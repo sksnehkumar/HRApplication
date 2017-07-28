9719ce5484e460211ea132ad08d6c005c3459173
sksnehkumar <sksnehkumar@gmail.com>
2017-07-28 10:39:52 +0530

    Added Database

---
+++ Common/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Common
+{
+    public static class Database
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return "server=.";
+            }
+        }
+
+        public const string CreateEmployee = "CreateEmployee"; 
+    }
+}
