50e5fd845ab040af89b320209bfbd10097542135
murarikrishna <krishna.murari34@gmail.com>
2017-07-28 12:12:54 +0530

    ProjectInfo

---
+++ BusinessEntities/ProjectInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    class ProjectInfo
+    {
+        public ProjectInfo()
+        {
+
+        }
+
+        public ProjectInfo(int projId, string projName, string description, string client, DateTime startDate, DateTime endDate, string createdBy, string lastModifiedBy)
+        {
+            ProjId = projId;
+            ProjName = projName;
+            Description = description;
+            Client = client;
+            StartDate = startDate;
+            EndDate = endDate;
+            CreatedBy = createdBy;
+            LastModifiedBy = lastModifiedBy;
+        }
+
+        public int ProjId { get; set; }
+        public string ProjName { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
