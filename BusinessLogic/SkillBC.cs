30a43128cd9dcfae3fc677124a794c92c026d218
sksnehkumar <sksnehkumar@gmail.com>
2017-07-30 13:12:59 +0530

    Added CategoryBC, ProjectBC, SkillBC

---
+++ BusinessLogic/SkillBC.cs
@@ -0,0 +1,42 @@
+﻿using BusinessEntities;
+using DataAccessLayer;
+using System.Data;
+
+
+namespace BusinessLogic
+{
+    public class SkillBC
+    {
+        public SkillBC()
+        {
+
+        }
+        public SkillBC(SkillDAL objSkillDAL)
+        {
+            this.objSkillDAL = objSkillDAL;
+        }
+
+        public SkillDAL objSkillDAL { get; set; }
+
+        public bool CreateSkill(SkillInfo objSkillInfo)
+        {
+            return objSkillDAL.CreateSkill(objSkillInfo);
+        }
+        public bool UpdateSkill(SkillInfo objSkillInfo)
+        {
+            return objSkillDAL.UpdateSkill(objSkillInfo);
+        }
+        public DataTable SearchSkills()
+        {
+            return objSkillDAL.SearchSkills();
+        }
+        public DataTable ViewSkill(int SkillId)
+        {
+            return objSkillDAL.ViewSkill(SkillId);
+        }
+        public DataTable GetSkillList()
+        {
+            return objSkillDAL.GetSkillList();
+        }
+    }
+}
