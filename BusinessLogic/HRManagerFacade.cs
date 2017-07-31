3f61835b968ee6b34969abd94f81fc37c4266bc3
SanjitVarnwal <kumarsanjit299@gmail.com>
2017-07-31 09:50:07 +0530

    HRManagerFacade

---
+++ BusinessLogic/HRManagerFacade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessEntities;
+
+namespace BusinessLogic
+{
+    class HRManagerFacade
+    {
+        EmployeeBC eBC;
+        CategoryBC cBC;
+        ProjectBC pBC;
+        SkillBC sBC;
+
+
+        public HRManagerFacade()
+        {
+            
+        }
+        public bool ValidateUser()
+        {
+            // TO DO : Define this function
+            return true;
+
+        }
+        public bool CreateEmployee( EmployeeInfo objEmployeeInfo )
+        {
+            return eBC.CreateEmployee(objEmployeeInfo);
+        }
+        
+
+    }
+}
