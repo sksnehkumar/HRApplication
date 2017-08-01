63b07f9839d6f764f6c9697e7b8df114ab57402c
sksnehkumar <sksnehkumar@gmail.com>
2017-08-01 11:37:44 +0530

    Skill Management

---
+++ WebUI/ViewSkill.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Controller;
+using BusinessEntities;
+
+namespace WebUI
+{
+    public partial class UpdateSkill : System.Web.UI.Page
+    {
+        SkillController objSC = new SkillController();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(Request.QueryString.ToString().Substring(8));
+            SkillInfo objSkill = objSC.viewSkill(402);
+            lblId.Text = objSkill.SkillId.ToString();
+            lblName.Text = objSkill.SkillName;
+            lblDescription.Text = objSkill.SkillDescription;
+            lblCategory.Text = objSkill.CategoryId.ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("SearchSkill.aspx");
+        }
+    }
+}
